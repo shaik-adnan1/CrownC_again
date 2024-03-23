4eeb79920ffcc91562ad874f7c01760f47c6dd9c
shaik-adnan1 <yousufwrk0@gmail.com>
2024-03-23 17:28:44 +0530

    firebase/firestore setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./components/routes/home/home.component";
 import Navigation from "./components/routes/navigation/navigation.component";
+import SignIn from "./components/routes/sign-in/sign-in.component";
 
 
 const Shop = () => {
@@ -15,6 +16,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
